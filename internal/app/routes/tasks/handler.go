b2745a91f38818c10496e201fbc3607dcafa205a
yigit433 <yigit433.devs@gmail.com>
2025-01-26 16:01:13 +0300

    feat: implement task handler with CRUD operations and update router setup

--- internal/app/routes/tasks/handler.go
+++ internal/app/routes/tasks/handler.go
@@ -1,21 +1,87 @@
 package tasksrouter
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/yigit433/go-rest-template/internal/app/model"
+	"github.com/yigit433/go-rest-template/internal/app/repository"
 )
 
-func GetTasks(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("GetTasks"))
+type TaskHandler struct {
+	Repo *repository.TaskRepository
 }
 
-func CreateTask(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("CreateTask"))
+func NewTaskHandler(repo *repository.TaskRepository) *TaskHandler {
+	return &TaskHandler{Repo: repo}
 }
 
-func UpdateTask(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("UpdateTask"))
+func (h *TaskHandler) GetTasks(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	tasks, err := h.Repo.FindAll(ctx)
+	if err != nil {
+		http.Error(w, "Error fetching tasks", http.StatusInternalServerError)
+		return
+	}
+	
+	json.NewEncoder(w).Encode(tasks)
 }
 
-func DeleteTask(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("DeleteTask"))
+func (h *TaskHandler) CreateTask(w http.ResponseWriter, r *http.Request) {
+	var task model.Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	ctx := context.Background()
+	taskId, err := h.Repo.Create(ctx, &task)
+	if err != nil {
+		http.Error(w, "Error creating task", http.StatusInternalServerError)
+		return
+	}
+
+	task.ID = &taskId
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(task)
+}
+
+func (h *TaskHandler) UpdateTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "Missing ID", http.StatusBadRequest)
+		return
+	}
+
+	var task model.Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+	ctx := context.Background()
+	if err := h.Repo.Update(ctx, id, &task); err != nil {
+		http.Error(w, "Error updating task", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Task updated"))
+}
+
+func (h *TaskHandler) DeleteTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "Missing ID", http.StatusBadRequest)
+		return
+	}
+	
+	ctx := context.Background()
+	if err := h.Repo.Delete(ctx, id); err != nil {
+		http.Error(w, "Error deleting task", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Task deleted"))
 }
--- internal/app/routes/tasks/setup.go
+++ internal/app/routes/tasks/setup.go
@@ -1,12 +1,21 @@
 package tasksrouter
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+
+	"github.com/yigit433/go-rest-template/internal/app/repository"
+	"github.com/yigit433/go-rest-template/internal/database"
+)
 
 func SetupRouter(r *chi.Mux) {
+    taskRepository := repository.NewTaskRepository(database.Mongo.TasksCollection)
+    
+    taskHandler := NewTaskHandler(taskRepository)
+
 	r.Route("/tasks", func(r chi.Router) {
-		r.Get("/", GetTasks)
-		r.Post("/", CreateTask)
-		r.Put("/{id}", UpdateTask)
-		r.Delete("/{id}", DeleteTask)
+		r.Get("/", taskHandler.GetTasks)
+		r.Post("/", taskHandler.CreateTask)
+		r.Put("/{id}", taskHandler.UpdateTask)
+		r.Delete("/{id}", taskHandler.DeleteTask)
 	})
 }
